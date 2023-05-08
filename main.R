001a8ea81924af8bc9d924562901b89ada19ba60
Amandine Vidal <amandine.vidalhosteng@gmail.com>
2023-05-08 15:08:24 +0200

    add and delete

---
+++ main.R
@@ -0,0 +1,8 @@
+#### -----------------------------------------------------------------####
+#### Author : Amandine Vidal-Hosteng
+#### Encoding : UTF-8
+#### Email : a.y.vidal-hosteng@rug.nl
+#### File path : colonization/main.R
+#### 
+#### Main file: contains the R code for the individual based model
+#### -----------------------------------------------------------------####
